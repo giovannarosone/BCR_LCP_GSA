321918de35f190c700ded0327b5cbb81be0bc354
Giovanna Rosone <giovyros@virgilio.it>
2018-11-02 18:01:40 +0100

    Update dependencies.mk

--- dependencies.mk
+++ dependencies.mk
@@ -1,5 +1,5 @@
-BCRexternalBWT.o: BCRexternalBWT.cpp BCRexternalBWT.h Tools.h Sorting.h TransposeFasta.h
-BWTCollection.o: BWTCollection.cpp BWTCollection.h Tools.h
+BCRexternalBWT.o: BCRexternalBWT.cpp BCRexternalBWT.h Tools.h Sorting.h TransposeFasta.h Parameters.h
+BWTCollection.o: BWTCollection.cpp BWTCollection.h Tools.h  Parameters.h
 Tools.o: Tools.cpp Tools.h
 Sorting.o: Sorting.cpp Sorting.h
 TransposeFasta.o: TransposeFasta.cpp TransposeFasta.h
