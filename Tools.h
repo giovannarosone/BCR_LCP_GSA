3e987701a22a62b22d25fc3b49688a09c88bfeae
Giovanna Rosone <giovyros@virgilio.it>
2018-11-02 18:06:31 +0100

    Update Tools.h

--- Tools.h
+++ Tools.h
@@ -38,6 +38,7 @@
 
 #include <iostream>
 #include <fstream>
+#include "Parameters.h"
 
 //#include <ctime>
 
@@ -46,63 +47,8 @@
 
 #define SIZE_ALPHA 256   
 
-/*
-#ifndef uchar
-#define uchar unsigned char
-#endif
-#ifndef uint
-#define uint unsigned int
-#endif
-#ifndef ulong
-#define ulong unsigned long
-#endif
-*/
-
-typedef unsigned char uchar;
-typedef unsigned int uint;
-typedef unsigned long ulong;
-
-#define dataTypedimAlpha uchar  //size of the alphabet (in biologic case 6 ($,A,C,G,N,T))
-#define dataTypelenSeq uint		//length of the sequences (in biologic case 100)
-#define dataTypeNumSeq 0		//number of sequences in the input file. If =0 -> uint
-#define dataTypeNumChar 1		//numer of characters in the input file (length of the BWT) If =0 -> uint
-
-#if dataTypeNumSeq == 1
-#   define dataTypeNSeq ulong
-#else
-#   define dataTypeNSeq uint
-#endif
-
-#if dataTypeNumChar == 1
-#   define dataTypeNChar ulong
-#else
-#   define dataTypeNChar uint
-#endif
-
-#define printEGSA 0
-
-#define verboseEncode 0
-#define verboseDecode 0
-#define deletePartialBWT 0
-#define deletePartialSA 0
-
 #define BackByVector 1
-#define BUILD_LCP 1
-#define BUILD_SA 0
 
-#define BCR_FROMCYC	0			//if BCR_FROMCYC=0 then BCR builds the cyc files before, otherwise BCR does not build the cyc files.
-#define BCR_SET 1				//if BCR_SET=1 then BCR computes the EBWT (set) (one can have string of different length in input, so BCR uses the symbol TERMINATE_CHAR_LEN) else BCR_SET=0 the BWT (single sequence)
-#define BCR_INPUT_IN_MEMORY 0	//if BCR_INPUT_IN_MEMORY==1 loads the input file in a string and compute the BWT of the string,  else it reads from file and computes the BWT of the reverse string.
-
-#define BUILD_EXT_MEMORY  1 //if BUILD_EXT_MEMORY==1, BCR uses files, if BUILD_EXT_MEMORY==0, BCR uses strings
-
-//Output of EGSA of Felipe Louze
-#define BUILD_BCR_FROM_EGSA 0 	//if BUILD_BCR_FROM_EGSA == 1, BCR takes in input the output of EGSA and add the symbols of new symbols. It produces the BCR output, i.e. file .pairSA, .bwt, .lcp, etc. (called in BCRexternalBWT)
-//if BUILD_BCR_FROM_EGSA == 1, BCR needs of the file outputFileName.occ that contains the occurrences of each symbols in the BWT already computed.
-
-#define OUTPUT_FORMAT_EGSA 0 //if OUTPUT_FORMAT_EGSA == 1, the output format of BCR is as the output of EGSA. BUILD_LCP and BUILD_SA and BUILD_EXT_MEMORY must be 1
-
-#define OUTPUT_linear_SuffixArray 0
 
 #if ((BUILD_BCR_FROM_EGSA == 1) || (OUTPUT_FORMAT_EGSA == 1))
 
@@ -127,10 +73,10 @@ typedef unsigned long ulong;
 
 
 //struct __attribute__((__packed__)) ElementType {
-struct ElementType {
-	dataTypelenSeq sa;          //It is the position in the sequence, so it goes from 0 a length read
-	dataTypeNSeq numSeq;		//It is the number of the sequence.
-};
+//struct ElementType {
+//	dataTypelenSeq sa;          //It is the position in the sequence, so it goes from 0 a length read
+//	dataTypeNSeq numSeq;		//It is the number of the sequence.
+//};
 
 class Tools
 {
