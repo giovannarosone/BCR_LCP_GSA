6f668d6be5fc31cf940ae87c24cbf40e4b84a87f
Giovanna Rosone <giovyros@virgilio.it>
2019-12-09 10:12:41 +0100

    Add files via upload

--- Script/2partial_eBWT_BCR.cpp
+++ Script/2partial_eBWT_BCR.cpp
@@ -29,7 +29,7 @@ using namespace std;
 #define BUFFERLCPSIZE 1024
 
 
-bool findLengthNseq( const string& input, const string& fileOutput);
+bool findLengthNseq( const string& input, const string& fileOutput, dataTypeNSeq, dataTypeNChar);
 
 int main(int argc, char **argv) {
     
@@ -45,6 +45,8 @@ int main(int argc, char **argv) {
     string fileFasta = argv[1];
     string fileOutput = argv[2];
     
+	dataTypeNSeq numTotSeq=0;
+	dataTypeNChar lenghtBWT=0;
     
     string fnLCP, fnDA, fnBWT;
     fnLCP = fileFasta+".4.lcp\0";
@@ -95,10 +97,11 @@ int main(int argc, char **argv) {
     
     
     //First reading in order to find the alphabet
+    std::cerr << "find the alphabet" << std::endl;
     uchar bwt;
     uchar c;
     dataTypelenSeq lcp;
-    dataTypeNSeq nSeq;
+    dataTypeNSeq idSeq;
     numcharBWT = fread(&bwt,sizeof(dataTypedimAlpha),1, InBWT);
     
 	#if BUILD_LCP == 1
@@ -107,7 +110,7 @@ int main(int argc, char **argv) {
 		dataTypelenSeq minLCP=lcp, maxLCP=lcp;
 	#endif
 	#if BUILD_DA == 1
-		numcharDA = fread(&nSeq,sizeof(dataTypeNSeq),1,InDA);
+		numcharDA = fread(&idSeq,sizeof(dataTypeNSeq),1,InDA);
 		assert(numcharBWT==numcharDA);
     #endif
     if ((unsigned int)bwt == 0)
@@ -115,18 +118,23 @@ int main(int argc, char **argv) {
     else
         c=bwt;
     freq[(unsigned int)(c)]=1;
+	numEle++;
     
+	dataTypeNSeq numSeq=0;
+	
     while ( fread(&bwt,sizeof(dataTypedimAlpha),1, InBWT) > 0 )   {
         //std::cerr << "bwt\tlcp\tpos\tnumSeq\tSA\tQS\n";
 		#if BUILD_LCP == 1
 			numcharLCP = fread(&lcp,sizeof(dataTypelenSeq),1,InLCP);
 		#endif
 		#if BUILD_DA == 1
-			numcharDA = fread(&nSeq,sizeof(dataTypeNSeq),1,InDA);
+			numcharDA = fread(&idSeq,sizeof(dataTypeNSeq),1,InDA);
 		#endif
         //std::cerr << "-->" << numEle << "\t" << GSA.text << "\t" << GSA.suff <<  "\t(" << GSA.lcp <<  "," << minLCP << ")\t" << c << "\n";
-        if ((unsigned int)bwt == 0)
+        if ((unsigned int)bwt == 0) {
             c=TERMINATE_CHAR;
+			numSeq++;
+		}
         else
             c=bwt;
 
@@ -142,28 +150,47 @@ int main(int argc, char **argv) {
         numEle++;
     }//end-while
     
-    
+	
     //Check
     assert(feof(InBWT));
     #if BUILD_LCP == 1
-        fread(&lcp,sizeof(dataTypelenSeq),1,InLCP);
+        int num = fread(&lcp,sizeof(dataTypelenSeq),1,InLCP);
+		if (!feof(InLCP)) {
+			std::cerr << "lcp: " << lcp << " num " << num << "\n";
+		}
         assert(feof(InLCP));
     #endif
     #if BUILD_DA == 1
-        fread(&nSeq,sizeof(dataTypeNSeq),1,InDA);
+        int num1 = fread(&idSeq,sizeof(dataTypeNSeq),1,InDA);
+		if (!feof(InLCP)) {
+			std::cerr << "lcp: " << lcp << " num " << num1 << "\n";
+		}
         assert(feof(InDA));
     #endif
     
-    
+    dataTypedimAlpha *alpha = new dataTypedimAlpha[SIZE_ALPHA];;  //Corresponding between alpha[i] and the symbol as char
     dataTypedimAlpha sizeAlpha=0;
-    for (dataTypedimAlpha i = 0; i < SIZE_ALPHA-1; ++i)
+    for (dataTypedimAlpha i = 0; i < SIZE_ALPHA-1; i++)
         if (freq[i] > 0) {
-            sizeAlpha++;
+			alpha[i] = sizeAlpha;
+            sizeAlpha++;			
         }
     if (freq[SIZE_ALPHA-1] > 0) {
+		alpha[SIZE_ALPHA-1] = sizeAlpha;
         sizeAlpha++;
     }
     
+	std::cerr << "Compute tableocc with where the size of the alphabet is " << (int)sizeAlpha << "." << std::endl;
+	dataTypeNChar** tableOcc; //contains the number of occurrences of each symbol
+	tableOcc = new dataTypeNChar*[sizeAlpha];
+	//Counting for each pile, es. $-pile, A-pile, C-pile, G-pile, N-pile, T-pile
+	for (dataTypedimAlpha j = 0 ; j < sizeAlpha; j++) {
+			tableOcc[j] = new dataTypeNChar[sizeAlpha];
+	  }
+	for (dataTypedimAlpha j = 0 ; j < sizeAlpha; j++)
+		for (dataTypedimAlpha h = 0 ; h < sizeAlpha; h++)
+			tableOcc[j][h]=0;
+	
     /*
     alphaInverse.resize(sizeAlpha);
     sizeAlpha=0;
@@ -180,8 +207,8 @@ int main(int argc, char **argv) {
     }
     */
     
-    std::cerr << "We supposed that the " <<numEle << " symbols in the input file are (ASCII, char, freq, code):\n";
-    for (dataTypedimAlpha i = 0; i < SIZE_ALPHA-1; ++i)
+    std::cerr << "There are " <<numEle << " symbols in the input file (ASCII, char, freq, code):\n";
+    for (dataTypedimAlpha i = 0; i < SIZE_ALPHA-1; i++)
         if (freq[i] > 0)
             std::cerr << (unsigned int)i << " " << i << " " << freq[i] << "\n";
     if (freq[SIZE_ALPHA-1] > 0)
@@ -206,7 +233,7 @@ int main(int argc, char **argv) {
     #if BUILD_DA == 1
         char *fnOutDA = new char[fileOutput.size()+100];
         static FILE *OutFileDA;
-        fseek(InDA,sizeof(dataTypeNSeq), SEEK_SET);
+        fseek(InDA,0, SEEK_SET);
     #endif
     /*
     #if BUILD_SA == 1
@@ -217,12 +244,14 @@ int main(int argc, char **argv) {
     numEle=0;
     numRead=0;
     numWrite=0;
-    minLCP=0;
+	#if BUILD_LCP == 1
+		minLCP=0;
+	#endif
     //std::cout << "nSeq" << "\t" << "Pos" << "\t" <<  "LCP" << "\t" <<  "minLCP" << "\t" <<  "BWT" << "\t" << "\n";
     
     for (dataTypedimAlpha currentPile = 0 ; currentPile < SIZE_ALPHA-1; currentPile++) {
         
-        if (freq[currentPile] > 0) {
+		if (freq[currentPile] > 0) {
             sprintf (fnOutBWT,"%s%s%d",fileOutput.c_str(),".ebwt_",currentPile);
             OutFileBWT = fopen(fnOutBWT, "wb");
             if (OutFileBWT==NULL) {
@@ -257,22 +286,38 @@ int main(int argc, char **argv) {
              */
             //std::cerr << "bwt\tlcp\tpos\tnumSeq\tSA\tQS\n";
             
-            for (dataTypeNChar j = 0 ; j < freq[currentPile]; j++) {
+			for (dataTypeNChar j = 0 ; j < freq[currentPile]; j++) {
                 numcharBWT = fread(&bwt,sizeof(dataTypedimAlpha),1, InBWT);
-                if ((unsigned int)bwt == 0)
+                if ((unsigned int)bwt == 0) {
                     c=TERMINATE_CHAR;
+					numTotSeq++;	
+				}
                 else
                     c=bwt;
-                fwrite(&c, sizeof(uchar), 1, OutFileBWT);
+				lenghtBWT++;
+                numWrite = fwrite(&c, sizeof(uchar), 1, OutFileBWT);				
+				assert(numcharBWT == numWrite);
+								
+				tableOcc[alpha[(unsigned int)currentPile]][alpha[(unsigned int)c]]++;      
+				//counting the number of occurrences in BWT of the $-pile
+
+				
+				
                 #if BUILD_LCP == 1
                     numcharLCP = fread(&lcp,sizeof(dataTypelenSeq),1,InLCP);
                     assert(numcharLCP==numcharBWT);
-                    fwrite (&lcp, sizeof(dataTypelenSeq), 1 , OutFileLCP);
+                    numWrite = fwrite (&lcp, sizeof(dataTypelenSeq), 1 , OutFileLCP);
+					assert(numcharLCP == numWrite);
                 #endif
                 #if BUILD_DA == 1
-                    numcharDA = fread(&nSeq,sizeof(dataTypeNSeq),1,InDA);
+                    numcharDA = fread(&idSeq,sizeof(dataTypeNSeq),1,InDA);
+					
+					if (numcharDA!=numcharBWT)
+						std::cerr <<  "numcharDA "<< numcharDA << " and numcharBWT" << numcharBWT << "\n";	
+					
                     assert(numcharDA==numcharBWT);
-                    fwrite (&nSeq, sizeof(dataTypeNSeq), 1 , OutFileDA);
+                    numWrite = fwrite (&idSeq, sizeof(dataTypeNSeq), 1 , OutFileDA);
+					assert(numcharDA == numWrite);
                 #endif
                 
                 /*
@@ -286,11 +331,13 @@ int main(int argc, char **argv) {
                 numEle++;
             }  //end-for
         } //end-if
+		//else
+
     }  //end-for
     
     
     
-    
+    //std::cout << "currentPile " << (int)SIZE_ALPHA-1 << "\n";
     if (freq[SIZE_ALPHA-1] > 0) {
         sprintf (fnOutBWT,"%s%s%d",fileOutput.c_str(),".ebwt_",SIZE_ALPHA-1);
         OutFileBWT = fopen(fnOutBWT, "wb");
@@ -340,9 +387,9 @@ int main(int argc, char **argv) {
                 fwrite (&lcp, sizeof(dataTypelenSeq), 1 , OutFileLCP);
             #endif
             #if BUILD_DA == 1
-                numcharDA = fread(&nSeq,sizeof(dataTypeNSeq),1,InDA);
+                numcharDA = fread(&idSeq,sizeof(dataTypeNSeq),1,InDA);
                 assert(numcharDA==numcharBWT);
-                fwrite (&nSeq, sizeof(dataTypeNSeq), 1 , OutFileDA);
+                fwrite (&idSeq, sizeof(dataTypeNSeq), 1 , OutFileDA);
             #endif
             /*
             #if BUILD_SA == 1
@@ -357,7 +404,7 @@ int main(int argc, char **argv) {
     } //end-if
     
     
-    std::cerr <<  " The total number of elements is " << numEle << "\n";
+    std::cerr <<  "The total number of elements is " << numEle << "\n";
     
     fclose(InBWT);
     fclose(OutFileBWT);
@@ -376,12 +423,61 @@ int main(int argc, char **argv) {
     #endif
     */
     
+    std::cout << "Store info files\n";
+	char *fnAux = new char[fileOutput.size()+100];
+	sprintf (fnAux,"%s%s",fileOutput.c_str(),".info\0");
+	FILE* OutFile = fopen(fnAux, "wb");
+	if (OutFile==NULL) {
+		std::cerr << "(lengthBWT+NSequences+sizeAlpha) Error opening " << fnAux << std::endl;
+		exit (EXIT_FAILURE);
+	}
+	
+	fwrite(&lenghtBWT,sizeof(dataTypeNChar),1,OutFile);
+	fwrite(&numTotSeq,sizeof(dataTypeNSeq),1,OutFile);
+	fwrite(&sizeAlpha,sizeof(dataTypedimAlpha),1,OutFile);
+	fclose(OutFile);
+	delete [] fnAux;
+	
+	
+	std::cout << "Store tableOcc files\n";
+	char *fnAuxTable = new char[fileOutput.size()+100];
+	sprintf (fnAuxTable,"%s%s",fileOutput.c_str(),".table\0");
+	FILE* OutFileTable = fopen(fnAuxTable, "wb");
+	if (OutFileTable==NULL) {
+		std::cerr << "(tableOcc) Error opening " << fnAuxTable << std::endl;
+		exit (EXIT_FAILURE);
+	}
+	std::cerr << "Writing tableOcc:\n";
+	for (dataTypedimAlpha j = 0 ; j < sizeAlpha; j++) {
+		std::cerr << (int)j << ": ";
+		for (dataTypedimAlpha h = 0 ; h < sizeAlpha; h++) {
+			dataTypeNChar num = tableOcc[j][h];
+			fwrite(&num,sizeof(dataTypeNChar),1,OutFileTable);
+			std::cerr <<   tableOcc[j][h] << " \t";
+		}
+		std::cerr << std::endl;
+	}
+	
+	fclose(OutFileTable);
+	
+	
+	for (dataTypedimAlpha j = 0 ; j < sizeAlpha; j++) {
+		delete [] tableOcc[j];
+		tableOcc[j] = NULL;
+	}
+	delete [] tableOcc;
+	
+	delete [] fnAuxTable;
     
     return 0;
 }
 
-bool findLengthNseq( const string& input, const string& fileOutput)
+bool findLengthNseq( const string& input, const string& fileOutput, dataTypeNSeq *nSeq, dataTypeNChar *lengthTexts)
 {
+	
+	//dataTypeNSeq nSeq = 0;
+    //dataTypeNChar lengthTexts = 0;       //Total length of all texts without $-symbols
+	
     std::ifstream infile(input.c_str());
     
     if (infile.is_open() == false) {
@@ -408,8 +504,7 @@ bool findLengthNseq( const string& input, const string& fileOutput)
     //Find max length and number of reads
     dataTypelenSeq charsNumber=0;
     dataTypelenSeq lengthRead=0;
-    dataTypeNSeq nSeq = 0;
-    dataTypeNChar lengthTexts = 0;       //Total length of all texts without $-symbols
+    
     
     while (getline(infile, bufChar))  {
         
@@ -421,21 +516,21 @@ bool findLengthNseq( const string& input, const string& fileOutput)
         //std::cerr << "Nuova lettura: seq:" << bufChar << ". Length " << bufChar.length() << " the last char is "<< (char)bufChar[bufChar.length()-1] << std::endl;
         
         if( bufChar[0] == '>' ) {     //it is the title of fasta file
-            nSeq++;
+            (*nSeq)++;
             //std::cerr << "--Length Sequence N. " << (int)(nSeq) << " is " << (int)charsNumber << std::endl;
             if (charsNumber != 0) {
                 //charsNumber--;
                 if (lengthRead != charsNumber) {
                     if (lengthRead < charsNumber)
                         lengthRead = charsNumber;
-                    if (nSeq > 2)
+                    if (*nSeq > 2)
                         lenSeq = true;
                 }
                 
                 //dataTypeNSeq numchar = fwrite (&charsNumber, sizeof(dataTypelenSeq), 1 , OutFileLen);
                 //assert( numchar == 1); // we should always read the same number of characters
                 lengthSeqVector.push_back (charsNumber);
-                lengthTexts += charsNumber;
+                *lengthTexts += charsNumber;
                 //if ((verboseEncode==1) || (verboseDistance ==1))
                 //std::cerr << "Length Sequence N. " << (int)(nSeq-1) << " is " << (int)charsNumber << " nSeq= "<< nSeq << std::endl;
             }
@@ -459,7 +554,7 @@ bool findLengthNseq( const string& input, const string& fileOutput)
         lenSeq = true;
     }
     lengthSeqVector.push_back (charsNumber);
-    lengthTexts += charsNumber;
+    *lengthTexts += charsNumber;
     
     if (lenSeq == true)
         cerr << "The  (new and-or old) reads have a different length." << endl;
@@ -469,10 +564,10 @@ bool findLengthNseq( const string& input, const string& fileOutput)
     //Store vector in file
     dataTypeNSeq numchar = fwrite (&lengthSeqVector[0], sizeof(dataTypelenSeq), lengthSeqVector.size(), OutFileLen);
     
-    std::cerr << "lengthTexts= " << (lengthTexts) << " lengthRead= " << (int)lengthRead << std::endl;
-    std::cerr << "lengthSeqVector.size= " << (lengthSeqVector.size()) << " nSeq= " << nSeq << std::endl;
+    std::cerr << "lengthTexts= " << (*lengthTexts) << " lengthRead= " << (int)lengthRead << std::endl;
+    std::cerr << "lengthSeqVector.size= " << (lengthSeqVector.size()) << " nSeq= " << *nSeq << std::endl;
     assert (lengthSeqVector.size() == numchar);
-    assert (lengthSeqVector.size() == nSeq);
+    assert (lengthSeqVector.size() == *nSeq);
     
     lengthSeqVector.clear();
     //lengthSeqVector.shrink_to_fit();
