72116a2839ffef8c3cac595529c0c0cf85854ed1
Giovanna Rosone <giovyros@virgilio.it>
2023-07-24 17:27:42 +0200

    Update BCR_BWTCollection.cpp
    new prints on standard output

--- BCR_BWTCollection.cpp
+++ BCR_BWTCollection.cpp
@@ -64,6 +64,7 @@ int main(int argc, char *argv[])
             exit(1);
         }
         inputPrevBCR = argv[3];
+        std::cerr << "WARNING!: BUILD_BCR_FROM_BCRpartials==1 - The types must be equal to previous BCR partial files (see BUILD_BCR_FROM_BCRpartials in Parameters.h)" << endl;
     #endif
     
 	
@@ -83,6 +84,210 @@ int main(int argc, char *argv[])
 		} 
 	#endif
 
+	#if ( FASTQ==1) 
+        if (USE_QS==0) {
+            std::cerr << "Error: if FASTQ==1 then USE_QS==1 (see USE_QS in Tools.h)" << endl;
+            exit (EXIT_FAILURE);
+        }
+    #endif
+
+    #if ( (USE_QS==1) &&  (FASTQ==0) )
+        std::cout << "The title of the quality score file must start with '{'. Please use the script.\n";
+    #endif
+
+    #if BCR_SET == 0        //Build BCR for 1 sequence
+        std::cout << "BCR of 1 sequence" << endl;
+
+        if ( (BUILD_DA_bit == 1) || (BUILD_DA == 1)  )  {
+            std::cout << "Error! Since BCR_SET == 0, then BUILD_DA or BUILD_DA_bit must be set to 0 (see Parameters.h).\n";
+            exit (EXIT_FAILURE);
+        }
+        
+        #if BCR_FROMCYC==1
+            std::cerr << "Error: BCR_FROMCYC==1 (cyc files in input) is not implemented! (see BCR_FROMCYC in Parameters.h)" << endl;
+            exit (EXIT_FAILURE);
+        #endif
+        
+        #if USE_QS==1
+            std::cerr << "Error: USE_QS==1 (quality score is not implemented! (see USE_QS in Tools.h)" << endl;
+            exit (EXIT_FAILURE);
+        #endif
+            
+
+        #if BUILD_BCR_FROM_BCRpartials==1
+            std::cerr << "Error: BUILD_BCR_FROM_BCRpartials==1 is not implemented! (see BUILD_BCR_FROM_BCRpartials in Parameters.h)" << endl;
+            exit (EXIT_FAILURE);
+        #endif
+
+        #if BCR_FROMCYC==1
+            std::cerr << "Error: BCR_FROMCYC==1 is not implemented! (see BCR_FROMCYC in Parameters.h)" << endl;
+            exit (EXIT_FAILURE);
+        #endif
+
+        #if BCR_INPUT_IN_MEMORY==1      // BCR reads from string
+            std::cout << "loads the input file in a string and compute the BWT of the string." << std::endl;
+        #else                           // BCR reads from file
+            std::cout << "reads from file and computes the BWT of the reverse string." << std::endl;
+        #endif
+    #else
+        std::cout << "BCR of multi-sequences" << endl;
+        #if BCR_INPUT_IN_MEMORY==1      // BCR reads from string
+            std::cerr << "Error BCR_SET == 0, so that BCR_INPUT_IN_MEMORY must be 0." << std::endl;
+            exit (EXIT_FAILURE);
+        #endif
+        #if BCR_FROMCYC==1 && KSEQ_PARSER==1
+            std::cerr << "Error: BCR_FROMCYC==1 (cyc files in input) is not implemented! (Please, set KSEQ_PARSER == 0)" << endl;
+            exit (EXIT_FAILURE);
+        #endif 
+    #endif
+    
+    
+    #if OUTPUT_linear_SuffixArray ==  1
+        std::cout << "dataTypeNChar: sizeof(type of #characters): " << sizeof(dataTypeNChar) << "\n";
+    #endif
+
+
+        if ( (BCR_SET!=1) && (BCR_SET_ALN_RH) && (BUILD_BCR_FROM_BCRpartials!=0) && (BUILD_BCR_ALTERNATE!=0) && (BUILD_LCP!=1) )
+        {
+            std::cerr << "Error! The input is a set. BCR_SET must be set to 1, BUILD_BCR_FROM_BCRpartials must be set to 0, BUILD_BCR_ALTERNATE must be set to 1  (see Parameters.h).\n";
+            exit (EXIT_FAILURE);
+        }
+        
+        if(BCR_SET_ALN_RH!=1 && BUILD_SAP )
+        {
+            std::cerr << "Error! BUILD_SAP works if BCR_SET_ALN_RH is set to 1 (see Parameters.h).\n";
+            exit (EXIT_FAILURE);
+        }
+         
+        if (OUTPUT_FORMAT==0) {
+            std::cout << "The output format of BCR is at most 5 files (ebwt, lcp, da, posSA, ebwt.qs) - built one after the other.\n";
+        }
+        
+        if (OUTPUT_FORMAT==1) {
+            if ((BUILD_LCP==1) && (BUILD_DA==1) && (BUILD_SA==1) )
+                std::cout << "The output format of BCR is as the output of eGSA.\n";
+            else {
+                std::cerr << "Error! The output format of BCR is as the output of EGSA. BUILD_LCP and BUILD_SA and BUILD_DA must be set to 1 (see Parameters.h).\n";
+                exit (EXIT_FAILURE);
+            }
+        }
+
+        if (OUTPUT_FORMAT==2) {
+            if ((BUILD_LCP==1) )
+                std::cout << "The output format of BCR is a unique file .egsa. Order: bwt, lcp, da, sa.\n";
+            else {
+                std::cerr << "Error! The output format of BCR is a unique file .egsa (we do not use a struct). BUILD_LCP must be set to 1  (see Parameters.h), BUILD_DA and BUILD_SA could be set to a either 0 or 1.  Order: bwt, lcp, da, sa.\n";
+                exit (EXIT_FAILURE);
+            }
+        }
+
+        if (OUTPUT_FORMAT==3) {
+            if ( ((BUILD_LCP == 1) || (BUILD_DA==1) || (BUILD_SA==1) || (BUILD_SAP==1) || KEEP_eBWT_IN_EXT_MEMORY==1) )
+                std::cout << "The output format of BCR is at most 5 files (ebwt, lcp, da, posSA, SAP-array) at the same time.\n";
+            else {
+                std::cerr << "Error! The output format of BCR is at most 4 files (ebwt, lcp, da, posSA) at the same time\n";
+                std::cerr << " BUILD_LCP or BUILD_DA==1 or BUILD_SA==1 or KEEP_eBWT_IN_EXT_MEMORY must be 1 (see Parameters.h).\n";
+                exit (EXIT_FAILURE);
+            }
+        }
+        
+        if (OUTPUT_FORMAT==4) {
+            if (BUILD_DA==1) 
+                std::cout << "The output format of BCR is at most 3 files (ebwt, da), lcp, sa.\n";
+            else {
+                std::cerr << "Error! The output format of BCR is at most 3 files (ebwt, da), lcp, sa. BUILD_DA must be set to 1 (see Parameters.h).\n";
+                exit (EXIT_FAILURE);
+            }
+        }
+        
+        if (OUTPUT_FORMAT==5) {
+            if ( (BUILD_DA==1) && (BUILD_LCP==1) )
+                std::cout << "The output format of BCR is at most 3 files ebwt, (lcp, da), sa.\n";
+            else {
+                std::cerr << "Error! The output format of BCR is at most 3 files ebwt, (lcp, da), sa. BUILD_DA and BUILD_LCP must be set to 1 (see Parameters.h).\n";
+                exit (EXIT_FAILURE);
+            }
+        }
+        
+        if (OUTPUT_FORMAT==6) {
+            if ( (BUILD_DA==1) && (BUILD_SA==1) )
+                std::cout << "The output format of BCR is at most 3 files ebwt), lcp, (sa, da).\n";
+            else {
+                std::cerr << "Error! The output format of BCR is at most 3 files ebwt, lcp, (sa, da). BUILD_DA and BUILD_SA must be set to 1 (see Parameters.h).\n";
+                exit (EXIT_FAILURE);
+            }
+        }
+
+    std::cout << "********************" << endl;
+    std::cout << "Setting:\n";
+    std::cout << "\tSpecial symbols: " << std::endl;
+    std::cout << "\t\tTERMINATE_CHAR: " << TERMINATE_CHAR << " Ascii: " << (int)TERMINATE_CHAR << std::endl;
+    std::cout << "\t\tTERMINATE_CHAR_LEN: " << TERMINATE_CHAR_LEN << " Ascii: " << (int)TERMINATE_CHAR_LEN <<  std::endl;
+
+    std::cout << "\tTypes:\n";
+    std::cout << "\t\tdataTypedimAlpha: sizeof(type size of alpha): " << sizeof(dataTypedimAlpha) << " bytes \n";
+    std::cout << "\t\tdataTypelenSeq: sizeof(type of seq length): " << sizeof(dataTypelenSeq) << " bytes \n";
+    std::cout << "\t\tdataTypeNSeq: sizeof(type of #sequences): " << sizeof(dataTypeNSeq) << " bytes\n";
+    std::cout << "\t\tdataTypeNChar: sizeof(type of #sequences): " << sizeof(dataTypeNChar) << " bytes\n";
+    #if OUTPUT_FORMAT == 1
+        std::cerr << "EGSA: sizeof(type of t_GSA): " << sizeof(t_GSA) << "\n";
+        std::cerr << "file EGSA: " << lengthTot_plus_eof * sizeof(t_GSA) << "\n";
+    #endif
+
+
+    std::cout << "\tAlphabet order:\n";
+    #if BUILD_BCR_ALTERNATE == 0
+        std::cout << "\t\tLexicographic order" << endl;
+    #else
+        std::cout << "\t\tAlternate lexicographic order" << endl;
+    #endif
+
+    std::cout << "\tEBWT computation (.ebwt file)" << endl;
+
+    #if (BUILD_LCP==1)
+        std::cout << "\t\ttogether LCP array (.lcp file)" << endl;
+    #endif
+
+    #if (BUILD_SA==1)
+        std::cout << "\t\ttogether SA array, i.e. positions in the sequence, so they go from 0 to sequence length (.posSA file)" << endl;
+    #endif
+
+    #if (BUILD_DA==1)
+        std::cout << "\t\ttogether DA array, i.e. ID/Colour of the sequence (.DA file)" << endl;
+    #endif
+
+    #if (BUILD_DA_bit==1) 
+        std::cout << "\t\ttogether DA (bit vector) of the sets, i.e. ID/Colour of two sets (.bitDA file)" << endl;
+    #endif
+
+    #if USE_QS == 1
+        std::cout << "\t\ttogether QS permutation" << endl;
+    #endif
+
+    std::cout << "\tBWT partial:\n";
+    #if  KEEP_eBWT_IN_EXT_MEMORY == 1    //BCR uses the internal memory for the BWT partial
+        std::cout << "\t\tin external memory" << endl;
+    #else
+        std::cout << "\t\tin internal memory" << endl;
+    #endif
+
+    std::cout << "\tInput file:\n";
+    #if  BCR_INPUT_IN_MEMORY == 0    //BCR reads cyc files
+        std::cout << "\t\tin external memory, uses .cyc files by using ";
+        if(BCR_SET_ALN_RH==0 )
+            std::cout << "LEFT alignment" ;
+        else
+            std::cout << "RIGHT alignment" ;
+        if(BCR_FROMCYC==0 )
+            std::cout << " and builds the cyc files" << endl;
+        else
+            std::cout << " and uses pre-computed cyc files" << endl;
+    #else
+        std::cout << "\t\tin internal memory (no cyc files)" << endl;
+    #endif
+
+    std::cout << "End Setting" << endl;
+    std::cout << "********************" << endl;
 
 	BWTCollection *BCRexternalBWT = BWTCollection::InitBWTCollection(argv[1], argv[2], inputPrevBCR );
 	
