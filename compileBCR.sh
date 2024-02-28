dfbb176aa554158018cfff22aaa50ecf75a05ae7
Giovanna Rosone <giovyros@virgilio.it>
2024-02-28 16:54:46 +0100

    Update compileBCR.sh

--- compileBCR.sh
+++ compileBCR.sh
@@ -2,27 +2,39 @@
 #!/bin/bash
 
 make 
-mv BCR_LCP_GSA BCR_LCP_GSA_original
+mv BCR_LCP_GSA BCR_inputBWT
 make clean
 
 make RLO=1
-mv BCR_LCP_GSA BCR_LCP_GSA_RLO
+mv BCR_LCP_GSA BCR_rloBWT
 make clean
 
 make SAP_INVERSE=1
-mv BCR_LCP_GSA BCR_LCP_GSA_inverse
+mv BCR_LCP_GSA BCR_altBWT
 make clean
 
 make SAP_PLUS=1
-mv BCR_LCP_GSA BCR_LCP_GSA_plus
+mv BCR_LCP_GSA BCR_plusBWT
 make clean
 
 make SAP_RANDOM=1
-mv BCR_LCP_GSA BCR_LCP_GSA_random
+mv BCR_LCP_GSA BCR_randBWT
 make clean
 
 make SAP=1
-mv BCR_LCP_GSA BCR_LCP_GSA_sap
+mv BCR_LCP_GSA BCR_sapArray
 make clean
 
-g++ dataSapIntervalTypeTwo.cpp -o dataSapIntervalTypeTwo
+make LCP=1
+mv BCR_LCP_GSA BCR_LCP
+make clean
+
+make DA=1
+mv BCR_LCP_GSA BCR_DA
+make clean
+
+make LCP=1 DA=1
+mv BCR_LCP_GSA BCR_LCP_DA
+make clean
+
+g++ stastisticsSAPintervals.cpp -o stastisticsSAPintervals
