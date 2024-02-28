a9b77da06fafd4b9b67ea02aced1b8f9eacfa306
Giovanna Rosone <giovyros@virgilio.it>
2024-02-28 17:08:21 +0100

    Update BCR_BWTCollection.cpp

--- BCR_BWTCollection.cpp
+++ BCR_BWTCollection.cpp
@@ -235,7 +235,7 @@ int main(int argc, char *argv[])
     std::cout << "\t\tdataTypeNSeq: sizeof(type of #sequences): " << sizeof(dataTypeNSeq) << " bytes\n";
     std::cout << "\t\tdataTypeNChar: sizeof(type of #sequences): " << sizeof(dataTypeNChar) << " bytes\n";
     std::cout << "\t\sortElement: sizeof(type of sortElement): " << sizeof(sortElement) << " bytes\n";
-    std::cout << "\t\vectTriple: sizeof(type of vectTriple): " << sizeof(vectTriple) << "bytes\n";
+    
     #if OUTPUT_FORMAT == 1
         std::cerr << "EGSA: sizeof(type of t_GSA): " << sizeof(t_GSA) << "\n";
         std::cerr << "file EGSA: " << lengthTot_plus_eof * sizeof(t_GSA) << "\n";
